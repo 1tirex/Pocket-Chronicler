835aa61c650e00f759bc79b8d331af474c54abfc
1tirex <it.rexball@gmail.com>
2023-01-10 16:39:44 +0400

    Initial Commit

---
+++ Pocket ChroniclerUITests/Pocket_ChroniclerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pocket_ChroniclerUITestsLaunchTests.swift
+//  Pocket ChroniclerUITests
+//
+//  Created by Дмитрий Собин on 10.01.23.
+//
+
+import XCTest
+
+final class Pocket_ChroniclerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
